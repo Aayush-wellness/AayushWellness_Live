9a13de1efb3dc2ab855dc27ae9f538107b6b7abc
Aayush-wellness <aayushwellnessltd@gmail.com>
2025-09-19 19:16:52 +0530

    Update Slider.js

--- src/Slider.js
+++ src/Slider.js
@@ -1004,10 +1004,9 @@ export default function AnimatedSlider() {
 
 
            {/* Overlay Content */}
-      <div className="absolute inset-0 flex flex-col justify-end pb-24 md:pb-32 px-4 sm:px-8 bg-gradient-to-t via-black/30 to-transparent"
+        <div className="absolute inset-0 flex flex-col justify-end pb-24 md:pb-32 px-4 sm:px-8 bg-gradient-to-t via-black/30 to-transparent"
         style={{
-          background-image=url("https://cdn.shopify.com/s/files/1/0636/5226/6115/files/tile4020.png?v=1758020387"),
-          
+          backgroundImage: 'url("https://cdn.shopify.com/s/files/1/0636/5226/6115/files/tile4020.png?v=1758020387")',
           height: '100vh'
         }}>
             <div className="max-w-4xl mx-auto text-center w-full px-4 text-white ">
