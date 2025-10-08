7a8b9482021001df54df50cae39625d6cd4fa761
Aayush-wellness <aayushwellnessltd@gmail.com>
2025-10-08 17:13:15 +0530

    Update SkeletonLoader.js

--- src/SkeletonLoader.js
+++ src/SkeletonLoader.js
@@ -1,18 +1,15 @@
-// SkeletonLoader.js
-import React from 'react';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
-
-export default function SkeletonLoader() {
+const SkeletonLoader = () => {
   return (
-    <div className="skeleton-loader">
-      <Skeleton height={300} width="100%" />
-      <h1 className="skeleton-title">
-        <Skeleton height={40} width={200} />
-      </h1>
-      <div className="skeleton-content">
-        <Skeleton count={5} />
+    <div className="min-h-screen bg-gray-100 animate-pulse">
+      <div className="h-20 bg-gray-200"></div>
+      <div className="h-screen bg-gray-300"></div>
+      <div className="max-w-7xl mx-auto px-4 py-12">
+        <div className="h-64 bg-gray-200 rounded-lg mb-8"></div>
+        <div className="h-64 bg-gray-200 rounded-lg mb-8"></div>
+        <div className="h-64 bg-gray-200 rounded-lg"></div>
       </div>
     </div>
   );
-}
+};
+
+export default SkeletonLoader;
