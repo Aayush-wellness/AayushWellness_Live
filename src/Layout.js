022bf66e1e7014f1d8ccd0ccf53596cc1c68df73
Aayush-wellness <aayushwellnessltd@gmail.com>
2025-11-08 15:34:02 +0530

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,36 +1,16 @@
-import React, { Suspense } from "react";
-import HeaderLoad from "./HeaderLoad";
-import SectionFirst from "./SectionFirst";
-import SectionSecond from "./Second";
-import SectionThird from "./SectionThird";
-import SectionFour from "./SectionFour";
-import SectionFive from "./SectionFive";
-import Footer from "./Footer";
-import FooterCopyright from "./FooterCopyright";
-import SliderThird from "./SliderThird";
+import React from "react";
 import Sliding from "./Slider";
-import SectionSix from "./SectionSix";
-import SectionSixAbove from "./SectionSixAbove";
-import SectionMissionHome from "./SectionMissionHome";
-import NewsletterSignup from "./NewsLetter";
 import StackImage from "./StackImage";
 import HeroSection from "./HeroSection";
 import NewFooter from "./NewFooter";
 import GummyProductSlider from "./GummyProductSlider";
 import Header from './Header';
-
-
-import "./index.css";
-import SectionTrail from "./SectionTrail";
 import AwCard from "./AwCard";
 import AwCard2 from "./AwCard2";
-
-
-import CircularSlider from "./CircularSlider";
 import CleanEnergyHero from "./CleanEnergyHero";
 import HomeBlog from "./HomeBlog";
-import Banner from "./Banner";
 import Homedoctor from "./Homedoctor";
+import "./index.css";
 
 function Layout() {
   return (
